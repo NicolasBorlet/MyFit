067576a4a111090b02507e7d3456797ad66836e7
NicolasBorlet <nicolas.borlet7@gmail.com>
2024-12-02 09:11:42 +0100

    feat: add expo-sensors and update HomeScreen layout
    Add the `expo-sensors` package to enhance sensor capabilities. 
    Refactor `HomeScreen` to simplify the layout by removing 
    animated scrolling and displaying the current date. Update 
    the `Base` component to remove unnecessary animations and 
    optimize the SVG rendering. These changes improve code 
    readability and maintainability while enhancing user 
    experience with real-time date display.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,34 +1,27 @@
-'use dom';
-
 import Base from '@/assets/images/base';
 import '../../../global.css';
-import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
-import { View } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 
 export default function HomeScreen() {
-  const scrollProgress = useSharedValue(0);
-
-  const scrollHandler = useAnimatedScrollHandler({
-      onScroll: (event) => {
-          // Adjust these values based on when you want the animation to complete
-          const progress = Math.min(event.contentOffset.y / 200, 1);
-          scrollProgress.value = progress;
-      },
+  dateWithoutYear = new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
   });
 
   return (
-    <Animated.ScrollView
-      onScroll={scrollHandler}
+    <ScrollView
       scrollEventThrottle={16}
       style={{ flex: 1 }}
       contentContainerStyle={{ flexGrow: 1, backgroundColor: 'white' }}
     >
       <View className='flex-1'>
-        <Base scrollProgress={scrollProgress} />
+        <Base />
       </View>
-      <h1 className="text-3xl font-bold underline text-orange">
-        Hello world!
-      </h1>
-    </Animated.ScrollView>
+      <View>
+        <Text>Today, {dateWithoutYear}</Text>
+        <Text>Your activities</Text>
+      </View>
+    </ScrollView>
   );
 }
--- src/assets/images/base.tsx
+++ src/assets/images/base.tsx
@@ -1,44 +1,18 @@
 import { useWindowDimensions } from 'react-native';
 import Svg, { Defs, Path, RadialGradient, Stop } from 'react-native-svg';
-import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
-const AnimatedSvg = Animated.createAnimatedComponent(Svg);
-const AnimatedStop = Animated.createAnimatedComponent(Stop);
-
-export default function Base({ scrollProgress }: any) {
+export default function Base() {
     const screenWidth = useWindowDimensions().width
 
-    const pathData = `M0 0H${screenWidth}V139H${screenWidth * 0.786}L${screenWidth * 0.648} 153.251L${screenWidth * 0.637} 155.935L${screenWidth * 0.361} 155.648L${screenWidth * 0.352} 153.538L${screenWidth * 0.213} 139H0V0Z`;
-
     return (
-        <AnimatedSvg 
-            width={screenWidth} 
-            height="171" 
-            viewBox={`0 0 ${screenWidth} 171`} 
-            fill="none"
-        >
-            <Path 
-                opacity="0.2" 
-                d={pathData} 
-                fill="url(#paint0_radial_57_11166)"
-            />
+        <Svg width={screenWidth} height="187" viewBox={`0 0 ${screenWidth} 187`} fill="none">
+            <Path opacity="0.2" fillRule="evenodd" clipRule="evenodd" d={`M0 0H${screenWidth}V139H${screenWidth * 0.786}C${screenWidth * 0.737} 139 ${screenWidth * 0.69} 143.926 ${screenWidth * 0.648} 153.251L${screenWidth * 0.637} 155.935C${screenWidth * 0.552} 175.039 ${screenWidth * 0.446} 174.928 ${screenWidth * 0.361} 155.648L${screenWidth * 0.352} 153.538C${screenWidth * 0.31} 144.029 ${screenWidth * 0.262} 139 ${screenWidth * 0.213} 139H0V0Z`} fill="url(#paint0_radial_57_11166)"/>
             <Defs>
-                <RadialGradient 
-                    id="paint0_radial_57_11166" 
-                    cx="0" 
-                    cy="0" 
-                    r="1" 
-                    gradientUnits="userSpaceOnUse" 
-                    gradientTransform={`translate(${screenWidth/2} 176.394) rotate(89.538) scale(156.924 ${screenWidth * 0.839})`}
-                >
-                    <AnimatedStop stopColor="#F15223" offset="0" />
-                    <AnimatedStop 
-                        stopColor="white" 
-                        offset="1"
-                        stopOpacity={scrollProgress}
-                    />
-                </RadialGradient>
+            <RadialGradient id="paint0_radial_57_11166" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform={`translate(${screenWidth/2} 176.394) rotate(89.538) scale(156.924 ${screenWidth * 0.839})`}>
+            <Stop stopColor="#F15223"/>
+            <Stop offset="1" stopColor="white"/>
+            </RadialGradient>
             </Defs>
-        </AnimatedSvg>
+        </Svg>
     )
 }
